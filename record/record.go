7498868616f3e5502caa02d40a9ee8654b9c007c
Andrew McWatters <andrew@andrewmcwatters.com>
2022-04-11 21:15:34 -0700

    go fmt

--- record/record.go
+++ record/record.go
@@ -1,13 +1,13 @@
 package main
 
 func main() {
-	records := make([]Record, 68453040);
+	records := make([]Record, 68453040)
 
 	for i := 0; i < len(records); i++ {
 		r := Record{}
 
 		r.id = i
-		records[i] = r;
+		records[i] = r
 	}
 }
 
