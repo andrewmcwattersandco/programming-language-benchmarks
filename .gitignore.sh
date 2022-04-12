f04aa5be13e77b622804171f100149af37a56876
Andrew McWatters <andrew@andrewmcwatters.com>
2022-04-12 08:12:56 -0700

    Update .gitignore.sh

--- .gitignore.sh
+++ .gitignore.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
 printf '' > .gitignore
 languages="C C++ Go Java Node Lua"
+
 for i in ${languages}
 do
     curl -o- https://raw.githubusercontent.com/github/gitignore/main/${i}.gitignore >> .gitignore
+done
+
+for d in */
+do
+    file=$(basename ${d})
+    echo ${file}/${file} >> .gitignore
 done
