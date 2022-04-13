31ec6ee11c5e47f2431f7a44afa9e1f2f908fc72
Andrew McWatters <andrew@andrewmcwatters.com>
2022-04-13 12:00:56 -0700

    Create json.c

---
+++ json/json.c
@@ -0,0 +1,28 @@
+#include <dirent.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+static char *fext(const char *name)
+{
+	return strrchr(name, '.') + 1;
+}
+
+int main(int argc, char *argv[])
+{
+	DIR *dir;
+	struct dirent *dp;
+
+	if ((dir = opendir ("companyfacts")) == NULL) {
+		perror ("Cannot open companyfacts");
+		exit (1);
+	}
+
+	while ((dp = readdir (dir)) != NULL) {
+		if (strcmp(fext(dp->d_name), "json") == 0) {
+			printf("%s\n", dp->d_name);
+		}
+	}
+
+	return 0;
+}
