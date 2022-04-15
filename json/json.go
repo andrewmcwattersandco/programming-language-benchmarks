aaa7757dfab4561a809ef8f7616b423f0b217886
Andrew McWatters <andrew@andrewmcwatters.com>
2022-04-14 22:48:52 -0700

    Update json.go

--- json/json.go
+++ json/json.go
@@ -1,4 +1,46 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+)
+
 func main() {
+	dir := "companyfacts"
+	files, err := os.ReadDir(dir)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, file := range files {
+		if !strings.HasSuffix(file.Name(), ".json") {
+			continue
+		}
+
+		name := dir + "/" + file.Name()
+		file, err := os.Open(name) // For read access.
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		stat, err := file.Stat()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		jsonBlob := make([]byte, stat.Size())
+		_, err = file.Read(jsonBlob)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		var f interface{}
+		err = json.Unmarshal(jsonBlob, &f)
+		if err != nil {
+			fmt.Println("error:", err)
+		}
+	}
 }
