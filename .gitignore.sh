23bf11a9735688a54695284ad4f7c633d4d7b9db
Andrew McWatters <andrew@andrewmcwatters.com>
2022-04-17 08:47:04 -0700

    Preserve go.mod

--- .gitignore.sh
+++ .gitignore.sh
@@ -7,6 +7,11 @@ do
     curl -o - https://raw.githubusercontent.com/github/gitignore/main/${lang}.gitignore >> .gitignore
 done
 
+# go.mod
+touch $TMPDIR/.gitignore
+grep -v '^\*.mod' .gitignore > $TMPDIR/.gitignore
+mv $TMPDIR/.gitignore .gitignore
+
 for dir in */
 do
     file=$(basename ${dir})
