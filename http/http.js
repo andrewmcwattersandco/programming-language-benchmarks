d027eed212c24b4a7f7daeccf70d492e0bce0b8a
Andrew McWatters <andrew@andrewmcwatters.com>
2022-05-08 11:46:05 -0600

    Comment out `app.listen`

--- http/http.js
+++ http/http.js
@@ -6,6 +6,6 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+// app.listen(port, () => {
+//   console.log(`Example app listening on port ${port}`)
+// })
