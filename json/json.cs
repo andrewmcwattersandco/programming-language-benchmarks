e717fc6f8e1e860704e914bb539d80779e08eaeb
Andrew McWatters <andrew@andrewmcwatters.com>
2025-08-14 09:00:00 -0700

    Create json.cs

---
+++ json/json.cs
@@ -0,0 +1,47 @@
+using System;
+using System.IO;
+using System.Text.Json;
+
+class Program
+{
+    static int Main(string[] args)
+    {
+        const string directory = "companyfacts";
+
+        try
+        {
+            string[] files = Directory.GetFiles(directory, "*.json");
+
+            foreach (string filePath in files)
+            {
+                try
+                {
+                    string jsonContent = File.ReadAllText(filePath);
+                    JsonDocument.Parse(jsonContent);
+                }
+                catch (IOException ex)
+                {
+                    Console.Error.WriteLine($"json: can't read {filePath}: {ex.Message}");
+                    continue;
+                }
+                catch (JsonException ex)
+                {
+                    Console.Error.WriteLine($"json: can't parse {filePath}: {ex.Message}");
+                    continue;
+                }
+            }
+        }
+        catch (DirectoryNotFoundException)
+        {
+            Console.Error.WriteLine($"json: can't open {directory}");
+            return 1;
+        }
+        catch (UnauthorizedAccessException)
+        {
+            Console.Error.WriteLine($"json: can't access {directory}");
+            return 1;
+        }
+
+        return 0;
+    }
+}
