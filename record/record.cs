cb6191b6a844a6899e55f7e02e0c62bb8e934000
Andrew McWatters <andrew@andrewmcwatters.com>
2025-08-14 09:03:43 -0700

    Create record.cs

---
+++ record/record.cs
@@ -0,0 +1,27 @@
+using System;
+
+record Record(int Id);
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        const int nRecords = 67108864;
+
+        try
+        {
+            Record[] records = new Record[nRecords];
+
+            for (int i = 0; i < nRecords; i++)
+            {
+                Record r = new Record(i);
+                records[i] = r;
+            }
+        }
+        catch (OutOfMemoryException ex)
+        {
+            Console.Error.WriteLine($"record: out of memory: {ex.Message}");
+            Environment.Exit(1);
+        }
+    }
+}
