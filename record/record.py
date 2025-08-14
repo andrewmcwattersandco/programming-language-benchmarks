762640a78db2b7838af91106332d2918e49159f4
Andrew McWatters <andrew@andrewmcwatters.com>
2025-08-13 17:26:50 -0700

    Create record.py

---
+++ record/record.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+class Record:
+    def __init__(self, record_id):
+        self.id = record_id
+
+
+def main():
+    records = []
+
+    for i in range(112813858):
+        r = Record(i)
+        records.append(r)
+
+
+if __name__ == "__main__":
+    main()
